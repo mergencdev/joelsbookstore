97bd977f593cc4cfa1b4a50c38bc9c540d888064
Mehmet Emin Ergenc <mergencdev@gmail.com>
2024-03-11 00:24:37 +0300

    add: testing dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,4 +82,14 @@ dependencies {
     //Room
     implementation("androidx.room:room-runtime:$roomVersion")
     kapt("androidx.room:room-compiler:$roomVersion")
+
+    // Unit tests
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.1")
+
+    // Android tests
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.room:room-testing:2.4.0")
+    androidTestImplementation("org.robolectric:robolectric:4.6.1")
 }
